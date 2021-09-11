ed3ae6b2e8651b14cc4288660d099cd6b9a75e76
shalex86 <alex.sh86@mail.ru>
2021-09-12 01:52:30 +0800

    lesson2.4_8.py added

---
+++ lesson2.4_8.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+browser = webdriver.Chrome('C:\\Webdrivers\\chromedriver.exe')
+
+browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+flag = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), '$100')
+    )
+if flag:
+	button = browser.find_element_by_id("book")
+	button.click()
+	x = browser.find_element_by_id("input_value").text
+	y = calc(x)
+	answer = browser.find_element_by_id('answer')
+	answer.send_keys(y)
+	btn_solve = browser.find_element_by_id('solve')
+	btn_solve.click()
